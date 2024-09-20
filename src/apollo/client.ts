056ca093d29119e2be39267caae07a7e2d01a5b5
Aleksandr Krivoshein <megalistnn@gmail.com>
2024-09-20 10:27:37 +0400

    refactor: remove commented code

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,22 +1,5 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-// const authLink = new ApolloLink((operation, forward) => {
-//   const accessToken = JSON.parse(localStorage.getItem('tokenData') || '');
-
-//   operation.setContext(({ headers }: { headers: Headers }) => ({
-//     headers: {
-//       ...headers,
-//       authorization: `Bearer ${accessToken}`,
-//     },
-//   }))
-
-//   return forward(operation)
-// })
-// const httpLink = new HttpLink({
-//     uri: prodURI,
-// });
-
-
 const client = new ApolloClient({
 	uri: 'https://api.escuelajs.co/graphql',
 	cache: new InMemoryCache()
