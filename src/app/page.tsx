cd7dba77796558f53188cf7c365cfff3f30efab7
Aleksandr Krivoshein <77492375+Wood85@users.noreply.github.com>
2024-09-24 00:38:17 +0300

    remove const description in the page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,6 @@ import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo/client";
 import LoginFormPage from "@/components/LoginFormPage/LoginFormPage";
 
-
-export const description =
-  "A simple login form with email and password. The submit button says 'Sign in'."
-
 export default function Page() {
 
   return (
